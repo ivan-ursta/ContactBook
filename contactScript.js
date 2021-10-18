1a1f19ff2702b04b067f208b97580782df43bbcd
ivan-ursta <65234223+ivan-ursta@users.noreply.github.com>
2021-10-18 22:55:39 +0300

    Add Class
    class ContactsAndSocialNеtworks

--- contactScript.js
+++ contactScript.js
@@ -4,6 +4,12 @@ let inputPhone = document.getElementById("inputPhone");
 let editPhone = document.getElementById("editPhone");
 let inputAge = document.getElementById("inputAge");
 let editAge = document.getElementById("editAge");
+let inputTelegram = document.getElementById("inputTelegram");
+let editTelegram = document.getElementById("editTelegram");
+let inputInstagram = document.getElementById("inputInstagram");
+let editInstagram = document.getElementById("editInstagram");
+let inputFacebook = document.getElementById("inputFacebook");
+let editFacebook = document.getElementById("editFacebook");
 let add = document.getElementById("add");
 let submit = document.getElementById("submit");
 let output = document.getElementById('output');
@@ -16,25 +22,44 @@ let editBtn = document.getElementById("editBtn");
 let counterContact = 0;
 
 class ContactsBook {
+
+}
+
+class Contacts extends ContactsBook {
     constructor(name, phone, age) {
+        super();
         this.name = name;
         this.phone = phone;
         this.age = age;
     }
+}
 
+class ContactsAndSocialNеtworks extends Contacts {
+    constructor(name, phone, age, nicknameTelegram, nicknameInstagram, nicknameFacebook) {
+        super();
+        this.name = name;
+        this.phone = phone;
+        this.age = age;
+        this.nicknameTelegram = nicknameTelegram;
+        this.nicknameInstagram = nicknameInstagram;
+        this.nicknameFacebook = nicknameFacebook;
+    }
     getUserContainer() {
         return `<div class="outputContent" id="outContent${counterContact}">
         <button id="delete" onclick="deleteContact()">X</button>
         <button id="edit" onclick="editContact()">Edit</button>
-            <p id="pName">${this.name}</p>
-            <p id="pPhone">${this.phone}</p>
-            <p id="pAge">${this.age}</p>
+            <p id="pName">Имя: ${this.name}</p>
+            <p id="pPhone">Телефон: ${this.phone}</p>
+            <p id="pAge">Возраст: ${this.age}</p>
+            <p id="pAge">Telegram: ${this.nicknameTelegram}</p>
+            <p id="pAge">Instagram: ${this.nicknameInstagram}</p>
+            <p id="pAge">Facebook: ${this.nicknameFacebook}</p>
          </div>`;
     }
 }
 
 function CreateObject() {
-    let user = new ContactsBook(inputName.value, inputPhone.value, inputAge.value);
+    let user = new ContactsAndSocialNеtworks(inputName.value, inputPhone.value, inputAge.value, inputTelegram.value, inputInstagram.value, inputFacebook.value);
     counterContact++;
     output.insertAdjacentHTML('beforeend', user.getUserContainer());
 
@@ -86,8 +111,6 @@ add.onclick = function() {
 closeBtn.addEventListener("click", function() {
     modal.style.display = "none";
 })
-
-
 
 // Когда пользователь щелкает в любом месте за пределами модального, закройте его
 window.onclick = function(event) {
